1a8c11cdfeb8a9b9da5e030659b68b33aec55244
Samuel Nartey <narteyotuafosamuel@gmail.com>
2025-12-06 19:29:30 +0000

    initial commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name="Samuel Nartey Otuafo";
+const age=27;
+const favourite_programming_language="javascript";
+
+console.log("Name: ",name); 
+console.log("Age: ",age);
+console.log("FavouriteProgrammingLanguage: ",favourite_programming_language);
